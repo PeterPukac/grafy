b88b6373b3b3a17a1208323361d2c1a19ceb0b10
Peterpukac <pukac.peter@avsys.sk>
2022-02-22 16:00:55 +0100

    blabal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import  './App.css';
-import Topbar from './Topbar';
-import Chart from './Chart';
+import Main from './Main';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Topbar></Topbar>
-        <Chart></Chart>
-      </Router>
+        <Main/>
     </div>
   );
 }
