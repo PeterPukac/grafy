ffb5c1448e5a078d2376a14d9b3d9675d23a5dfe
Peterpukac <pukac.peter@avsys.sk>
2022-03-01 16:33:10 +0100

    opravenie vsetkych veci

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,7 +1,3 @@
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { FaDesktop } from 'react-icons/fa';
 import { BsLink45Deg } from 'react-icons/bs';
 import { BsPersonFill } from 'react-icons/bs';
@@ -10,168 +6,112 @@ import { FcButtingIn } from 'react-icons/fc';
 import { FaArrowAltCircleRight } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { isSameMonth, parseISO, } from 'date-fns';
+
+
 const Topbar = (props) => {
     const employeesArray = props.data;
     const orderArrays = props.orders;
-    const currentDate = new Date()
-    console.log(employeesArray);
-    const orderArrayInMonth = orderArrays.filter(order =>
-        isSameMonth(parseISO(order.date), currentDate));
+    const currentDate = new Date();
+    const arrayHours = [];
+    const iconSize = 50;
+    const iconsArray = [<FaDesktop size={iconSize} />, <BsLink45Deg size={iconSize} />, <BsPersonFill size={iconSize} />, <GiKnifeFork size={iconSize} />];
+    const headersArray = ["Počet objednávok za aktuálny mesiac", "Suma z objednávok za akutálny mesiac", "Zamestnanec mesiaca", "Obed"];
+    const colorsArray = ["rgb(255, 77, 77)", "rgb(255, 179, 26)", "rgb(179, 179, 179)", "rgb(89, 89, 89)"];
+    const orderArrayInMonth = orderArrays.filter(order => isSameMonth(parseISO(order.date), currentDate));
+    employeesArray.map(emp => { arrayHours.push(emp.hours); })
+    const pricesOrderArrays = orderArrays.filter(order => isSameMonth(parseISO(order.date), currentDate));
 
-    //vyfiltrovanie tych objednavok ktore su v danom mesiaci
-    const pricesOrderArrays = orderArrays.filter(order =>
-        isSameMonth(parseISO(order.date), currentDate));
+    
+    function getArrayOfCards(pHeadersArray, pColorsArray, pIconsArray,pCountOrderOfMonth,pSumOrderMonth,pEmpOfTheMonth) {
+        let arrayOfcards = [];
+        let valueForInsert;
+        for (let index = 0; index < pHeadersArray.length; index++) {
+            if(index==0){
+                valueForInsert = pCountOrderOfMonth;
+            }else if(index==1){
+                valueForInsert = pSumOrderMonth;
+            }else if(index == 2){
+                valueForInsert = pEmpOfTheMonth;
+            }else if(index==3){
+                valueForInsert = <FcButtingIn />;
+            }
+            let card = {
+                header: pHeadersArray[index],
+                color: pColorsArray[index],
+                icon: pIconsArray[index],
+                value: valueForInsert
+            }
+            let cardObj = Object.create(card);
+            arrayOfcards.push(cardObj);
+        }
+
+        return arrayOfcards;
+    }
 
     function getSumOfPriceMonth(pPricesOrderArrays) {
-        var sum = 0;
-        for (let index = 0; index < pPricesOrderArrays.length; index++) {
-            sum += pPricesOrderArrays[index].price;
+        let sum = 0;
+        for (let price of pPricesOrderArrays) {
+            sum += price.price;
         }
         return sum;
     }
-    var sumOrdersOfMonth = getSumOfPriceMonth(pricesOrderArrays);
-    //------------------------NAJDENIE ZAMESTNANCA MESIACA---------------
-    const arrayHours = [];
-    let max = 0;
-    //Naplnene pola hodin
-    employeesArray.map(emp => {
-        arrayHours.push(emp.hours);
-    })
-    //najdenie maxima v poli
-    for (let index = 0; index < arrayHours.length; index++) {
-        if (arrayHours[index] > max) {
-            max = arrayHours[index];
+
+    function getEmployeeOfTheMonth(pArrayHours, pEmployeesArray) {
+        let max = 0;
+        for (let hours of pArrayHours) {
+            if (hours > max) {
+                max = hours;
+            }
         }
+        let name = ' ';
+        let empOfMonth = [];
+        empOfMonth = pEmployeesArray.filter(emp => emp.hours === max);
+        empOfMonth.map(emp => {
+            name = emp.name;
+        })
+
+        return name;
     }
-    var name = ' ';
-    var empOfMonth = [];
-    empOfMonth = employeesArray.filter(emp => emp.hours === max);
-    empOfMonth.map(emp => {
-        name=emp.name;
-    })
-    //-------------------------KONIEC ZAMESTNANCA-----------------
+
+    let countOrdersOfMonth = orderArrayInMonth.length;
+    let sumOrdersOfMonth = getSumOfPriceMonth(pricesOrderArrays);
+    let employeeOfTheMonth = getEmployeeOfTheMonth(arrayHours, employeesArray);
+    let arrayCards = getArrayOfCards(headersArray, colorsArray, iconsArray,countOrdersOfMonth,sumOrdersOfMonth,employeeOfTheMonth);
+
     return (
         <div className="TopBar">
-            <Container fluid>
-                <Row>
-                    <Col sm={12} md={6} lg={3} >
-                        <Card className="card " style={{ backgroundColor: "rgb(255, 77, 77)" }}>
-                            <Card.Body>
-                                <Container fluid>
-                                    <Row>
-                                        <Col md={10}>
-                                            <Card.Title className="text-start firstCard" style={{
-                                                textTransform: "uppercase",
-                                                color: "white", fontSize: "10px"
-                                            }}>Počet objednávok za aktuálny mesiac</Card.Title>
-                                            <Card.Text className="text-start" style={{ color: "white", fontWeight: "bold" }} >
-                                                {orderArrayInMonth.length}</Card.Text>
-                                        </Col>
-                                        <Col md={2}>
-                                            <FaDesktop size={50} style={{ color: "rgb(38, 38, 38)", opacity: "0.3" }} />
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                <Card.Text className="text-end" style={{
-                                    backgroundColor: "rgb(38, 38, 38)",
-                                    marginTop: "10px",
-                                    opacity: "0.5", marginLeft: "-17px", marginRight: "-17px", marginBottom: "-18px"
-                                }}>
-                                    <a style={{ color: "white", fontSize: "12px", paddingRight: "5px" }}>Details <FaArrowAltCircleRight /></a>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col sm={12} md={6} lg={3}>
-                        <Card className="card " style={{ backgroundColor: "rgb(255, 179, 26)" }}>
-                            <Card.Body>
-                                <Container fluid>
-                                    <Row>
-                                        <Col md={10}>
-                                            <Card.Title className="text-start firstCard" style={{
-                                                textTransform: "uppercase",
-                                                color: "white", fontSize: "10px"
-                                            }}>Suma objednávok za aktuálny mesiac</Card.Title>
-                                            <Card.Text className="text-start" style={{ color: "white", fontWeight: "bold" }} >
-                                                {sumOrdersOfMonth}€
-                                            </Card.Text>
-                                        </Col>
-                                        <Col md={2}>
-                                            <BsLink45Deg size={50} style={{ color: "rgb(38, 38, 38)", opacity: "0.3" }} />
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                <Card.Text className="text-end" style={{
-                                    backgroundColor: "rgb(38, 38, 38)",
-                                    marginTop: "10px",
-                                    opacity: "0.5", marginLeft: "-17px", marginRight: "-17px", marginBottom: "-18px"
-                                }}>
-                                    <a style={{ color: "white", fontSize: "12px", paddingRight: "5px" }}>Detail <FaArrowAltCircleRight /></a>
-
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={12} md={6} lg={3}>
-                        <Card className="card " style={{ backgroundColor: "rgb(179, 179, 179)" }}>
-                            <Card.Body>
-                                <Container fluid>
-                                    <Row>
-                                        <Col md={10}>
-                                            <Card.Title className="text-start firstCard" style={{
-                                                textTransform: "uppercase",
-                                                color: "white", fontSize: "10px"
-                                            }}>Zamestnanec mesiaca </Card.Title>
-                                            <Card.Text className="text-start" style={{color:"white"}}>{name}
-                                            </Card.Text>
-                                        </Col>
-                                        <Col md={2}>
-                                            <BsPersonFill size={50} style={{ color: "rgb(38, 38, 38)", opacity: "0.3" }} />
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                <Card.Text className="text-end" style={{
-                                    backgroundColor: "rgb(38, 38, 38)",
-                                    marginTop: "10px",
-                                    opacity: "0.5", marginLeft: "-17px", marginRight: "-17px", marginBottom: "-18px"
-                                }}>
-                                    <a style={{ color: "white", fontSize: "12px", paddingRight: "5px" }}>Detail <FaArrowAltCircleRight /></a>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={12} md={6} lg={3}>
-                        <Card className="card " style={{ backgroundColor: "rgb(89, 89, 89)" }}>
-                            <Card.Body>
-                                <Container fluid>
-                                    <Row>
-                                        <Col md={10}>
-                                            <Card.Title className="text-start firstCard" style={{
-                                                textTransform: "uppercase",
-                                                color: "white", fontSize: "10px"
-                                            }}>Obed</Card.Title>
-                                            <Card.Text className="text-start" >
-                                                <FcButtingIn />
-                                            </Card.Text>
-                                        </Col>
-                                        <Col md={2}>
-                                            <GiKnifeFork size={50} style={{ color: "rgb(38, 38, 38)", opacity: "0.3" }} />
-                                        </Col>
-                                    </Row>
-                                </Container>
-                                <Card.Text className="text-end" style={{
-                                    backgroundColor: "rgb(38, 38, 38)",
-                                    marginTop: "10px",
-                                    opacity: "0.5", marginLeft: "-17px", marginRight: "-17px", marginBottom: "-18px"
-                                }}>
-                                    <a style={{ color: "white", fontSize: "12px", paddingRight: "5px" }}>Detail <FaArrowAltCircleRight /></a>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="container-fluid">
+                <div className="row">
+                    {arrayCards.map(card => {
+                        return (
+                            <div className=" col-sm-12 col-md-6 col-lg-3" key= {card.color} >
+                                <div className="card" style={{ backgroundColor: card.color }}>
+                                    <div className="card-body">
+                                        <div className="container-fluid">
+                                            <div className="row">
+                                                <div className="col-md-10">
+                                                    <div className="card-title">
+                                                        {card.header}
+                                                    </div>
+                                                    <div className="card-text" >
+                                                        {card.value}
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-2">
+                                                    <div className="icon">{card.icon}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="details">
+                                            <a >Details <FaArrowAltCircleRight /></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
