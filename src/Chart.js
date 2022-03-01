9cdbd0ebe8f3e2428b116e0c58cad28effc35738
Peterpukac <pukac.peter@avsys.sk>
2022-03-01 08:20:56 +0100

    daco

--- src/Chart.js
+++ src/Chart.js
@@ -1,8 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList, PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+import { Label, BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList, PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
 import { isAfter, parseISO, isBefore } from 'date-fns';
+import React from 'react';
 const Chart = (props) => {
     const data = props.data;
     data.sort((a, b) => (a.price < b.price) ? 1 : -1);
@@ -49,15 +50,26 @@ const Chart = (props) => {
 
     const COLORS = ['rgb(0, 179, 0)', 'rgb(204, 0, 0)'];
 
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, index }) => {
+        const radius = 20 + innerRadius + (outerRadius - innerRadius);
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {dataDva[index].name}
+            </text>
+        );
+    };
+
     return (
         <div className="chart">
             <Container fluid width="80%" aspect={3}>
-                <div className="textik">
-                    <h1 style={{ color: "white", fontSize: "15px" }}>Náklady na projekt</h1>
-                    <div style={{ color: "white", fontSize: "10px" }}>Rýchly náhľad nákladov na jednotlivé projekty podľa čísla</div>
-                </div>
+                <h1 style={{ color: "white", fontSize: "20px", paddingTop: "20px", textAlign: "left" }}>Náklady na projekt</h1>
+                <div style={{ color: "white", fontSize: "12px", color: "lightsteelblue", textAlign: "left" }}>Rýchly náhľad nákladov na jednotlivé projekty podľa čísla</div>
                 <Row>
-                    <Col sm={12} md={8} lg={10}>
+                    <Col sm={12} md={7} lg={9}>
                         <ResponsiveContainer>
                             <BarChart
                                 width={600}
@@ -70,34 +82,38 @@ const Chart = (props) => {
                                     bottom: 5,
                                 }}>
                                 <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="id" />
+                                <XAxis dataKey="id">
+                                    <div>Cislo projektu</div>
+                                    <Label value="Číslo projektu" offset={0} position="insideBottom"  fill="white"  />
+                                </XAxis>
                                 <YAxis />
                                 <Tooltip />
-                                <Legend />
+
                                 <Bar dataKey="price" fill="rgb(77, 121, 255)" >
-                                    <LabelList position="top" fill="white" fontSize={10} className="bar" />
+                                    <LabelList position="top" fill="white" fontSize={10} className="bar"></LabelList>
                                 </Bar>
                             </BarChart>
                         </ResponsiveContainer>
                     </Col>
-                    <Col sm={12} md={4} lg={2}>
+                    <Col sm={12} md={5} lg={3}>
                         <div className="textik">
-                            <div className="pomer">
+                            <div id="pomer">
                                 {projectsBeforeDeadline}  / {projectsAfterDeadline}
                             </div>
-                            <h1 style={{ color: "lightsteelblue", fontSize: "15px", fontWeight: "lighter" }}>Pomer projektov</h1>
+                            Pomer projektov
                         </div>
                         <PieChart width={800} height={400}>
                             <Pie
                                 data={dataDva}
-                                cx={120}
+                                cx={141}
                                 cy={200}
-                                innerRadius={60}
+                                innerRadius={50}
                                 outerRadius={80}
                                 fill="#8884d8"
                                 paddingAngle={5}
                                 dataKey="value"
-                            >
+                                isAnimationActive={false}
+                                label={renderCustomizedLabel}>
                                 {dataDva.map((entry, index) => (
                                     <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                 ))}
@@ -105,14 +121,11 @@ const Chart = (props) => {
                         </PieChart>
 
                         <div className="podGrafom">
-                            <div style={{ color: "white", fontSize: "30px" }}>
-                                <strong>V termíne: </strong>{projectsBeforeDeadline}
-                            </div>
-                            <div style={{ color: "white", fontSize: "30px" }}>
-                                <strong className="cervene">Meškajúce: </strong>{projectsAfterDeadline}
-                            </div>
+                            <div id="green"><strong> {projectsBeforeDeadline}</strong>V termíne</div>
+                            <div id="red"><strong> {projectsAfterDeadline}</strong> Meškajúce</div>
                         </div>
                     </Col>
+
                 </Row>
             </Container>
         </div >
