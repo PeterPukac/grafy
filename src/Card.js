3ae0ec89b23df2f83265254e9fcb14357baee50f
Peterpukac <pukac.peter@avsys.sk>
2022-03-02 08:21:08 +0100

    card spaveny ako osobitny komponent

---
+++ src/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaArrowAltCircleRight } from 'react-icons/fa';
+const Card = (props) => {
+    const object = props.object;
+    return (
+        <div className="card">
+            <div className="card" style={{ backgroundColor: object.color }}>
+                <div className="card-body">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-sm-10 col-md-10">
+                                <div className="card-title">
+                                    {object.header}
+                                </div>
+                                <div className="card-text" >
+                                    {object.value}
+                                </div>
+                            </div>
+                            <div className="col-sm-2 col-md-2">
+                                <div className="icon">{object.icon}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="details">
+                        <a >Details <FaArrowAltCircleRight /></a>
+                    </div>
+                </div>
+            </div>
+        </div >
+
+    )
+}
+
+export default Card;
--- src/Topbar.js
+++ src/Topbar.js
@@ -3,10 +3,9 @@ import { BsLink45Deg } from 'react-icons/bs';
 import { BsPersonFill } from 'react-icons/bs';
 import { GiKnifeFork } from 'react-icons/gi';
 import { FcButtingIn } from 'react-icons/fc';
-import { FaArrowAltCircleRight } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { isSameMonth, parseISO, } from 'date-fns';
-
+import Card from './Card';
 
 const Topbar = (props) => {
     const iconSize = 50;
@@ -23,7 +22,7 @@ const Topbar = (props) => {
         return sum;
     }
 
-    const  getEmployeeOfTheMonth = (pArrayHours, pEmployeesArray) => {
+    const getEmployeeOfTheMonth = (pArrayHours, pEmployeesArray) => {
         let max = 0;
         for (let hours of pArrayHours) {
             if (hours > max) {
@@ -46,10 +45,10 @@ const Topbar = (props) => {
     const sumOrdersOfMonth = getSumOfPriceMonth(pricesOrderArrays);
     const employeeOfTheMonth = getEmployeeOfTheMonth(arrayHours, employeesArray);
     const cards = [
-        {  header: "Počet objednávok za aktuálny mesiac", color:"rgb(255, 77, 77)" , icon:<FaDesktop size={iconSize} />, value: countOrdersOfMonth},
-        {  header: "Suma z objednávok za akutálny mesiac", color:"rgb(255, 179, 26)" , icon: <BsLink45Deg size={iconSize} />, value: sumOrdersOfMonth},
-        {  header: "Zamestnanec mesiaca", color:"rgb(179, 179, 179)" , icon: <BsPersonFill size={iconSize} />, value: employeeOfTheMonth},
-        {  header: "Obed", color:"rgb(89, 89, 89)" , icon: <GiKnifeFork size={iconSize} />, value: <FcButtingIn size={25}/> }
+        { header: "Počet objednávok za aktuálny mesiac", color: "rgb(255, 77, 77)", icon: <FaDesktop size={iconSize} />, value: countOrdersOfMonth },
+        { header: "Suma z objednávok za akutálny mesiac", color: "rgb(255, 179, 26)", icon: <BsLink45Deg size={iconSize} />, value: sumOrdersOfMonth },
+        { header: "Zamestnanec mesiaca", color: "rgb(179, 179, 179)", icon: <BsPersonFill size={iconSize} />, value: employeeOfTheMonth },
+        { header: "Obed", color: "rgb(89, 89, 89)", icon: <GiKnifeFork size={iconSize} />, value: <FcButtingIn size={25} /> }
     ];
 
     return (
@@ -58,29 +57,8 @@ const Topbar = (props) => {
                 <div className="row">
                     {cards.map(card => {
                         return (
-                            <div className=" col-sm-12 col-md-6 col-lg-3" key= {card.color} >
-                                <div className="card" style={{ backgroundColor: card.color }}>
-                                    <div className="card-body">
-                                        <div className="container-fluid">
-                                            <div className="row">
-                                                <div className="col-sm-10 col-md-10">
-                                                    <div className="card-title">
-                                                        {card.header}
-                                                    </div>
-                                                    <div className="card-text" >
-                                                        {card.value}
-                                                    </div>
-                                                </div>
-                                                <div className="col-sm-2 col-md-2">
-                                                    <div className="icon">{card.icon}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="details">
-                                            <a >Details <FaArrowAltCircleRight /></a>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className=" col-sm-12 col-md-6 col-lg-3" key={card.color} >
+                                <Card object={card} />
                             </div>
                         )
                     })}
